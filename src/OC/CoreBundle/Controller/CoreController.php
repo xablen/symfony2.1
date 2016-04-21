5c4c33c66320a91157ec55a687e1f4ed015ee734
xablen <xavier.parfait@gmail.com>
2016-04-21 10:12:31 +0200

    Manipulation d'Entity

--- src/OC/CoreBundle/Controller/CoreController.php
+++ src/OC/CoreBundle/Controller/CoreController.php
@@ -11,11 +11,7 @@ class CoreController extends Controller
   // La page d'accueil
   public function indexAction()
   {
-    // On retourne simplement la vue de la page d'accueil
-    // L'affichage des 3 dernières annonces utilisera le contrôleur déjà existant dans PlatformBundle
     return $this->get('templating')->renderResponse('OCCoreBundle:Core:index.html.twig');
-
-    // La méthode raccourcie $this->render('...') est parfaitement valable
   }
 
   // La page de contact
